586cecf5d21695104cf6b13b43710378c7d950e7
huanhust1998 <huanhust1998@gmail.com>
2024-03-17 10:28:52 +0700

    update: add UI for header

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -11,24 +11,24 @@ const MENU_ITEMS = [
 
 const Header = () => {
   return (
-    <div className="w-full px-60 flex justify-between items-center h-12">
+    <div className="fixed top-0 w-full px-60 flex justify-between items-center h-12 pt-10">
       <div className="flex items-center justify-center gap-1">
         <img src={logo} alt="logo" width={40} />
-        <h1 className="text-14 font-bold">Chatbot</h1>
+        <h1 className="text-16 font-bold">Chatbot</h1>
       </div>
-      <div className="flex gap-2">
+      <div className="flex gap-5">
         {MENU_ITEMS.map((item, key) => (
           <div key={key}>
-            <span className="text-14 text-gray-80 font-semibold">{item}</span>
+            <span className="text-16 text-grey-1000 font-medium">{item}</span>
           </div>
         ))}
       </div>
-      <div>
-        <span>
+      <div className="flex justify-center items-center gap-5">
+        <span className="text-16 text-grey-1000 font-semibold">
           Sign in
         </span>
-        <button>
-          <span>BOOK A MEETING</span>
+        <button className="flex items-center justify-center px-6 py-3 bg-pink-700 rounded-xl hover:bg-grey-1000 group duration-200" type="button">
+          <span className="text-16 text-grey-1000 font-semibold group-hover:text-white">BOOK A MEETING</span>
         </button>
       </div>
     </div>
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../layout/header";
+import Layout from "../../layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <>Home</>
+    </Layout>
+  );
+};
+export default Home;
